15fb56f1a231bfca79d9325ee65c7e6f9fb0701f
SugarBarrel <55982389+SugarBarrel@users.noreply.github.com>
2022-09-16 16:26:19 +0700

    Update Sidi's Resprite Project

--- website/src/mods.tsx
+++ website/src/mods.tsx
@@ -68,7 +68,7 @@ const list: ModProps[] = [
     title: "Sidi's Resprite Project",
     description: "Replaces vanilla sprites (all items and some object and character sprites) with more bulky and prettier ones.",
     buttons: [
-      {type:"direct", link:"https://cdn.discordapp.com/attachments/766891245141622804/943492887524564992/Sidis_resprite_project.spritepack"},
+      {type:"direct", link:"https://cdn.discordapp.com/attachments/453258186514890772/1020264085729849365/Sidis_resprite_project.spritepack"},
     ],
     tags: ["spritepack", "items", "objects", "npcs", "sprites", "innovative"],
   },
