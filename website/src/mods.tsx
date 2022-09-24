4d259b516a30c1f1321e565bf6244310ae27c4d4
SugarBarrel <55982389+SugarBarrel@users.noreply.github.com>
2022-09-24 14:00:45 +0700

    Update mods.tsx

--- website/src/mods.tsx
+++ website/src/mods.tsx
@@ -34,7 +34,7 @@ const list: ModProps[] = [
       </span>
     ),
     buttons: [
-      {type:"direct", link:"https://cdn.discordapp.com/attachments/453258186514890772/1022449982973935666/Sidis_resprite_project.spritepack"},
+      {type:"direct", link:"https://cdn.discordapp.com/attachments/453258186514890772/1023126744674340904/Sidis_resprite_project.spritepack"},
     ],
     tags: ["spritepack", "items", "objects", "npcs", "sprites", "innovative"],
   },
